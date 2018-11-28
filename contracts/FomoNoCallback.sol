9d48a09812cacdb8bba3387275bc8214ea5f0cbb
eitan <eitanlev@gmail.com>
2018-11-28 11:49:40 +0200

    added no callback FOMO contract and tests

---
+++ contracts/FomoNoCallback.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.4.24;
+
+contract FomoNoCallback {
+	address public lastPlayer;
+	uint public lastTime;
+	address public lastWinner;
+	uint public lastWinAmount;
+	uint public lastWinTime;
+	uint public currentWinAmount;
+	uint public constant feeValue = 1;
+	uint public gameTime = 1 minutes;
+	address public gameCreator;
+
+	event WinnerAnnouncement(address indexed _winner, uint indexed _amount, uint indexed _lastWinTime);
+
+	constructor() public {
+		lastPlayer = 0;
+		lastTime = now;
+		lastWinner = 0;
+		lastWinAmount = 0;
+		lastWinTime = 0;
+		currentWinAmount = 0;
+		gameCreator = msg.sender;
+	}
+
+	function buyTicket() public payable checkIfWon aboveThreshold {
+		currentWinAmount += msg.value - feeValue;
+		lastPlayer = msg.sender;
+		lastTime = now;
+	}
+
+	function announceWinner() public payable {
+		if (now > lastTime + gameTime*(1 seconds) && lastPlayer != 0) {
+			lastPlayer.transfer(currentWinAmount);
+			lastWinner = lastPlayer;
+			lastWinAmount = currentWinAmount;
+			currentWinAmount = 0;
+			lastPlayer = 0;
+			lastTime = now;
+			lastWinTime = now;
+			emit WinnerAnnouncement(lastWinner, lastWinAmount, lastWinTime);
+		}
+	}
+
+	function changeGameTime(uint timeInterval) public onlyMaster {
+		gameTime = timeInterval;
+	}
+
+	modifier onlyMaster() {
+		require(msg.sender == gameCreator, "Changing game time intervals is allowed only by the creator");
+		_;
+	}
+
+	modifier checkIfWon() {
+		announceWinner();
+		_;
+	}
+
+	modifier aboveThreshold() {
+		require(msg.value > feeValue, 'Not enough cryptocurrency was sent');
+		_;
+	}
+}
---
+++ test/FomoNoCallback.truffle.test.js
@@ -0,0 +1,87 @@
+const fomoContract = artifacts.require("./FomoNoCallback.sol");
+
+let FomoInstance;
+let Player1;
+let Player2;
+let Player3;
+let gameCreator;
+
+const ticket1 = 3;
+const ticket2 = 4;
+const ticket3 = 2;
+const feeValue = 1;
+let totalAmount = 0;
+const timeInterval = 10;
+
+contract("fomoNoCallback", function(accounts) {
+	gameCreator = accounts[0];
+	Player1 = accounts[1];
+	Player2 = accounts[2];
+	Player3 = accounts[3];
+
+	beforeEach('Deploying a new FOMO contract', async function() {
+		FomoInstance = await fomoContract.new({from: gameCreator});
+		totalAmount = 0;
+		await FomoInstance.changeGameTime(timeInterval, {from: gameCreator});
+	});
+
+	describe('E2E testing of FOMO contract', function() {
+		it('expecting Player 1 to win', async function() {
+			await FomoInstance.buyTicket({from: Player1, value: ticket1});
+			contractBalance = (await FomoInstance.currentWinAmount()).toNumber();
+			totalAmount = ticket1 - feeValue;
+			assert.equal(contractBalance, totalAmount);
+			await FomoInstance.buyTicket({from: Player2, value: ticket2});
+			contractBalance = (await FomoInstance.currentWinAmount()).toNumber();
+			totalAmount = totalAmount + ticket2 - feeValue;
+			assert.equal(contractBalance, totalAmount);
+			await FomoInstance.buyTicket({from: Player1, value: ticket3});
+			contractBalance = (await FomoInstance.currentWinAmount()).toNumber();
+			totalAmount = totalAmount + ticket3 - feeValue;
+			assert.equal(contractBalance, totalAmount);
+
+			await secondsSleep(timeInterval/2);
+			await FomoInstance.announceWinner({value: 3});
+			contractBalance = (await FomoInstance.currentWinAmount()).toNumber();
+			assert.equal(contractBalance, totalAmount);
+			lastWinAmount = (await FomoInstance.lastWinAmount()).toNumber();
+			assert.equal(lastWinAmount, 0);
+
+			await secondsSleep(timeInterval/2);
+			receipt = await FomoInstance.announceWinner({value: 3});
+			lastPlayer = (await FomoInstance.lastPlayer());
+			currentWinAmount = (await FomoInstance.currentWinAmount()).toNumber();
+			lastWinner = await FomoInstance.lastWinner();
+			lastWinAmount = (await FomoInstance.lastWinAmount()).toNumber();
+			assert.equal(lastPlayer, 0);
+			assert.equal(currentWinAmount, 0);
+			assert.equal(lastWinner, Player1);
+			assert.equal(lastWinAmount, totalAmount);
+			assert.equal(lastWinner, receipt.logs[0].args._winner);
+			assert.equal(lastWinAmount, receipt.logs[0].args._amount);
+			lastWinTime = receipt.logs[0].args._lastWinTime;
+			//anotherLastWinTime = await FomoInstance.lastWinTime();
+			//assert.equal(lastWinTime.toNumber(), anotherLastWinTime);
+
+			//assert.equal(lastWinTime.toNumber(), (await FomoInstance.lastWinTime()));
+			//assert.equal(lastWinTime, (await FomoInstance.lastWinTime).toNumber());
+
+			//await FomoInstance.announceWinner({value: 3});
+			//assert.equal(lastWinTime, await FomoInstance.lastWinTime());
+		});
+	});
+});
+
+
+let secondsSleep = async function(seconds) {
+	//await new Promise(resolve => setTimeout(resolve, (timeInterval*1000)/2));
+	await new Promise(resolve => setTimeout(resolve, seconds*1000));
+};
+
+
+
+
+
+
+
+
