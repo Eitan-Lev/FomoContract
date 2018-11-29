ad2290388b6d26b732ad1599f4d704fbfefa44bf
kfirtoledo <kfir.toledo@gmail.com>
2018-11-29 03:17:05 +0200

    [LastPayCheck]
    change names to Fomo projext

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,5 @@
-//const DepositFactory = artifacts.require("DepositFactory");
-//const SgxSimulator = artifacts.require("SGXSimulator");
-//const DepositConract = artifacts.require("Deposit");
-var Lastpay = artifacts.require("./Lastpay.sol");
+var Fomo = artifacts.require("./Fomo.sol");
 
-module.exports = async function(deployer, network, accounts) {
-	deployer.deploy(Lastpay);
-	//deployer.deploy(DepositFactory);
-	//deployer.deploy(SgxSimulator);
-	//var A = await deployer.deploy(DepositFactory);
-	//deployer.deploy(DepositConract, accounts[0], {from: A.address, value: 2});
+module.exports = function(deployer) {
+  deployer.deploy(Fomo);
 };
