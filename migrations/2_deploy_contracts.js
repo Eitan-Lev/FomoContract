c927e23a5401fc4cfb5e4b9a81e6cb96f2983b85
Ubuntu <eitan@TruffleVmBeta1.j4214zewd1dupfwjpgv4bepuuc.bx.internal.cloudapp.net>
2018-12-06 09:28:46 +0000

    revert and add contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,7 @@
-//const DepositFactory = artifacts.require("DepositFactory");
-//const SgxSimulator = artifacts.require("SGXSimulator");
-//const DepositConract = artifacts.require("Deposit");
-const FomoNoCallback = artifacts.require("FomoNoCallback");
+const Fomo = artifacts.require("./Fomo.sol");
+const FomoNoCallback = artifacts.require("./FomoNoCallback.sol");
 
-//module.exports = async function(deployer, network, accounts) {
 module.exports = function(deployer, network, accounts) {
+  deployer.deploy(Fomo);
 	deployer.deploy(FomoNoCallback);
-	//deployer.deploy(DepositFactory);
-	//deployer.deploy(SgxSimulator);
-	//var A = await deployer.deploy(DepositFactory);
-	//deployer.deploy(DepositConract, accounts[0], {from: A.address, value: 2});
 };
