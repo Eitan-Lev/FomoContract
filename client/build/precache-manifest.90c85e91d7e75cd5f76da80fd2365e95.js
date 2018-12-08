0c18e3908e235ee5094006c5e32390d51f8475a2
Eitan Levin <t-eilevi@microsoft.com>
2018-12-08 17:13:20 +0200

    with build files

---
+++ client/build/precache-manifest.90c85e91d7e75cd5f76da80fd2365e95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "58518080f02a912ecc69",
+    "url": "/static/js/main.58518080.chunk.js"
+  },
+  {
+    "revision": "faf4bc32b0bbf6868806",
+    "url": "/static/js/1.faf4bc32.chunk.js"
+  },
+  {
+    "revision": "58518080f02a912ecc69",
+    "url": "/static/css/main.217b3f44.chunk.css"
+  },
+  {
+    "revision": "ddde4604acde9b2bfe619addf60ec305",
+    "url": "/index.html"
+  }
+];
